393c59b2f884f6d7fa1ed504a26ffae4cff4103d
Teamexe786 <h86665460@gmail.com>
2025-08-02 22:27:35 +0530

    Update VideoPlaceholder.tsx

--- src/components/VideoPlaceholder.tsx
+++ src/components/VideoPlaceholder.tsx
@@ -2,16 +2,20 @@ import { useState } from 'react';
 import { Volume2, VolumeX } from 'lucide-react';
 
 const VideoPlaceholder = () => {
-  const [isMuted, setIsMuted] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
 
   const toggleMute = () => {
     const iframe = document.querySelector('iframe') as HTMLIFrameElement;
     if (iframe) {
       const currentSrc = iframe.src;
       if (isMuted) {
+        // Remove muted param
         iframe.src = currentSrc.replace('&muted=1', '');
       } else {
-        iframe.src = currentSrc + '&muted=1';
+        // Add muted param
+        if (!currentSrc.includes('&muted=1')) {
+          iframe.src = currentSrc + '&muted=1';
+        }
       }
       setIsMuted(!isMuted);
     }
@@ -20,19 +24,19 @@ const VideoPlaceholder = () => {
   return (
     <div className="container mx-auto px-6 py-8">
       <div className="video-container relative">
-        {/* Video iframe */}
-        <div style={{position:"relative", width:"100%", height:"0px", paddingBottom:"56.250%"}}>
-          <iframe 
-            allow="fullscreen;autoplay" 
-            allowFullScreen 
-            height="100%" 
-            src="https://streamable.com/e/a2p2bz?autoplay=1" 
-            width="100%" 
-            style={{border:"none", width:"100%", height:"100%", position:"absolute", left:"0px", top:"0px", overflow:"hidden"}}
+        {/* Updated Video iframe */}
+        <div style={{ position: "relative", width: "100%", height: "0px", paddingBottom: "56.250%" }}>
+          <iframe
+            allow="fullscreen"
+            allowFullScreen
+            height="100%"
+            src="https://streamable.com/e/9d8j5m?muted=1&nocontrols=1"
+            width="100%"
+            style={{ border: "none", width: "100%", height: "100%", position: "absolute", left: "0px", top: "0px", overflow: "hidden" }}
             className="rounded-lg"
           />
         </div>
-        
+
         {/* Mute/Unmute Button */}
         <button
           onClick={toggleMute}
@@ -41,8 +45,8 @@ const VideoPlaceholder = () => {
         >
           {isMuted ? <VolumeX size={20} /> : <Volume2 size={20} />}
         </button>
-        
-        {/* Corner decorations */}
+
+        {/* Corner Decorations */}
         <div className="absolute top-2 left-2 w-6 h-6 border-t-2 border-l-2 border-accent z-10"></div>
         <div className="absolute top-2 right-2 w-6 h-6 border-t-2 border-r-2 border-accent z-10"></div>
         <div className="absolute bottom-2 left-2 w-6 h-6 border-b-2 border-l-2 border-accent z-10"></div>
